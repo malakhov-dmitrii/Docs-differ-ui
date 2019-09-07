a6c6e6957a6a10217ca5fe0b03964c3aacd35cc7
guid21 <uryuk.uz@gmail.com>
2019-09-07 22:04:08 +0300

    added tree

--- src/features/diff-docs/DefaultPage.js
+++ src/features/diff-docs/DefaultPage.js
@@ -7,91 +7,107 @@ import AppBar from "@material-ui/core/AppBar";
 import { Card, Container, makeStyles, Toolbar, Tooltip, Typography } from "@material-ui/core";
 
 const data = [
-  {
-    type: "tag",
-    repeat: 1,
-    tag: "br"
-  },
-	{
-		type: "blank",
-		text: `You"re going to hear a genuine BBC news report from 28 June, 2016. Before you listen, read these three summaries:`
-	},
-  {
-    type: "tag",
-    repeat: 2,
-    tag: "br"
-  },
-	{
-		type: "blank",
-		text: `1. European leaders will be discussing Britain"s future relationship with the EU in meetings with the British prime minister.`
-	},
-  {
-    type: "tag",
-    repeat: 2,
-    tag: "br"
-  },
-	{
-		type: "blank",
-		text: `2. European leaders will not be discussing Britain"s future relationship with the EU in meetings with the British prime minister.
-        
-        3. `
-	},
-  {
-    type: "tag",
-    repeat: 2,
-    tag: "br"
-  },
-	{
-		type: "blank",
-		text: `2. European leaders will not be discussing Britain"s future relationship with the EU in meetings with the British prime minister.`
-	},
-  {
-    type: "tag",
-    repeat: 2,
-    tag: "br"
-  },
-	{
-		type: "blank",
-		text: `3. `
-	},
-	{
-        type: "choice",
-        value: null,
-        arr: [
-            {
-                date: new Date(23 * 3600 * 1000),
-                document: "one",
-                text: "European"
-            },
-            {
-                date: new Date(24 * 3600 * 1000),
-                document: "two",
-                text: "European"
-            }
-        ]
-    },
 	[
+    {
+      type: "blank",
+      text: `You"re going to hear a genuine BBC news report from 28 June, 2016. Before you listen, read these three summaries:`
+    }
+  ],
+  [
+    {
+      type: "tag",
+      tag: "b",
+      text: "1"
+    },
+    {
+      type: "tag",
+      text: `. European leaders will be discussing Britain"s future relationship with the EU in meetings with the British prime minister.`,
+      tag: "u"
+    }
+  ],
+  [
+    {
+      type: "tag",
+      tag: "b",
+      text: "2"
+    },
+    {
+      type: "tag",
+      text: `. European leaders will not be discussing Britain"s future relationship with the EU in meetings with the British prime minister.`,
+      tag: "i"
+    }
+  ],
+  [
+    {
+      type: "tag",
+      tag: "b",
+      text: "3"
+    },
+    {
+      type: "blank",
+      text: `. `
+    },
+    {
+      type: "choice",
+      value: null,
+      arr: [
+          {
+              date: new Date(23 * 3600 * 1000),
+              document: "one",
+              text: "European"
+          },
+          {
+              date: new Date(24 * 3600 * 1000),
+              document: "two",
+              text: "European"
+          }
+      ]
+    },
 		{
 			type: "tumblr",
-            date: new Date(23 * 3600 * 1000),
-            document: "one",
+      date: new Date(23 * 3600 * 1000),
+      document: "one",
 			switch: false,
-			data: {
-				type: "blank",
-				text: ` leaders will be starting the process of Britain leaving the EU in meetings with the British prime minister.
-
-                Step 2: Learn the key words and listen again
-                How was that? Try listening again. Here are three definitions of key vocabulary items which may help you.
-                
-                face
-                (here) deal with
-                
-                triggers
-                starts a process
-                
-                mechanism
-                (here) way of doing something; process`
-			}
+			data: [
+        {
+          type: "blank",
+          text: ` leaders will be starting the process of Britain leaving the EU in meetings with the British prime minister.`
+        },
+        {
+          type: "tag",
+          repeat: 2,
+          tag: "br"
+        },
+        {
+          type: "blank",
+          text: `Step 2: Learn the key words and listen again`
+        },
+        {
+          type: "tag",
+          repeat: 1,
+          tag: "br"
+        },
+        {
+          type: "blank",
+          text: ` How was that? Try listening again. Here are three definitions of key vocabulary items which may help you.`
+        },
+        {
+          type: "tag",
+          repeat: 2,
+          tag: "br"
+        },
+        {
+          type: "blank",
+          text: ` face
+                  (here) deal with
+                  
+                  triggers
+                  starts a process
+                  
+                  mechanism
+                  (here) way of doing something; process`
+        }
+      ]
 		},
 		{
 			type: "blank",
@@ -105,10 +121,15 @@ const data = [
             Answer
             2. European leaders will not be discussing Britain"s future relationship with the EU in meetings with the British prime minister.`
 		},
+    {
+      type: "tag",
+      repeat: 2,
+      tag: "br"
+    },
 		{
 			type: "tumblr",
-            date: new Date(24 * 3600 * 1000),
-            document: "two",
+      date: new Date(24 * 3600 * 1000),
+      document: "two",
 			switch: false,
 			data: [
 				{
@@ -149,8 +170,7 @@ const data = [
 				}
 			]
 		}
-
-	]
+  ]
 ];
 
 
@@ -175,27 +195,45 @@ export const DefaultPage = () => {
   const classes = useStyles();
 
   const renderTag = (data) => {
+    let repeat = [];
+
+    if (data.repeat) {
+      for (let i = 0; i < data.repeat; i++) {
+        repeat.push("1");
+      }
+    }
     switch(data.tag) {
       case "br":
-        return <br />;
+        return repeat.map(() => {
+          return <br />;
+        });
+      case "b":
+        return <b>{data.text}</b>;
+      case "i":
+        return <i>{data.text}</i>;
+      case "u":
+        return <u>{data.text}</u>;
+      default:
+        return "";
     }
   }
 
   const loop = (data) => {
-    console.log(data)
     if (Array.isArray(data)) {
       return data.map(item => {
         if ((typeof item == "object") && !Array.isArray(item)) {
           switch(item.type) {
             case "blank":
               return item.text;
+            case "tumblr":
+              return loop(item.data);
             case "tag":
               return renderTag(item);
             default:
               return "";
           }
         }else if (Array.isArray(item)) {
-          return loop(item);
+          return <p>{loop(item)}</p>;
         }
       });
     }
